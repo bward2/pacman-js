4b2357cd0cd478e0f775a78b9c6d5267ca70d029
Brent <d3wardbrent@gmail.com>
2018-10-11 21:03:26 -0500

    Refactoring to use gulp-concat

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,16 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var cssnano = require('gulp-cssnano');
-var gulpIf = require('gulp-if');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const concat = require('gulp-concat');
 
 gulp.task('sass', function(){
     return gulp.src('style/**/*.scss')
-        .pipe(sass()) // Converts Sass to CSS with gulp-sass
+        .pipe(sass())
+        .pipe(concat('app.css'))
         .pipe(gulp.dest('style'))
-        .pipe(gulpIf('*.css', cssnano()))
 });
 
 gulp.task('watch', function(){
     gulp.watch('style/**/*.scss', ['sass']);
 });
 
-gulp.task('default', ['watch']);
+gulp.task('default', ['sass', 'watch']);
