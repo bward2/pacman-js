b09da2de9ac023a99d83779655a036123b870ca6
Brent <d3wardbrent@gmail.com>
2018-10-14 21:49:18 -0500

    Restructuring project

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,14 @@ const sass = require('gulp-sass');
 const concat = require('gulp-concat');
 
 gulp.task('sass', function(){
-    return gulp.src('style/**/*.scss')
+    return gulp.src('app/style/scss/**/*.scss')
         .pipe(sass())
         .pipe(concat('app.css'))
-        .pipe(gulp.dest('style'))
+        .pipe(gulp.dest('build'));
 });
 
 gulp.task('compile-js', function(){
-    return gulp.src('scripts/**/*.js')
+    return gulp.src('app/scripts/**/*.js')
         .pipe(concat('app.js'))
         .pipe(gulp.dest('build'));
 });
@@ -19,4 +19,4 @@ gulp.task('watch', function(){
     gulp.watch('style/**/*.scss', ['sass']);
 });
 
-gulp.task('default', ['sass', 'watch']);
+gulp.task('default', ['sass', 'compile-js']);
