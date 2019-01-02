05ca6b96c0a8f9749ba5dff58ad6bc06c448de16
Brent <d3wardbrent@gmail.com>
2019-01-01 21:20:44 -0600

    Slightly improved gameCoordinator's tests

--- app/tests/gameCoordinator.test.js
+++ app/tests/gameCoordinator.test.js
@@ -3,33 +3,47 @@ const sinon = require('sinon');
 const GameCoordinator = require('../scripts/core/gameCoordinator');
 
 let gameCoordinator;
+const mazeArray = [
+    ['X','X','X'],
+    ['X','o',' '],
+    ['X',' ','X'],
+];
 
-beforeEach(()=> {
-    global.Pacman = class {};
-    global.CharacterUtil = class {};
-    global.Ghost = class {};
-    global.Pacdot = class {};
-    global.GameEngine = class {
-        start() { return; }
-    };
+describe('gameCoordinator', ()=> {
+    beforeEach(()=> {
+        global.Pacman = class {};
+        global.CharacterUtil = class {};
+        global.Ghost = class {};
+        global.Pacdot = class {};
+        global.GameEngine = class {
+            start() { return; }
+        };
+    
+        global.document = {
+            getElementById: ()=> {
+                return {
+                    appendChild: ()=> { return; }
+                };
+             },
+            createElement: ()=> {
+                return {
+                    classList: {
+                        add: ()=> { return; }
+                    },
+                    appendChild: ()=> { return; },
+                    style: {}
+                };
+            }
+        };
+    
+        gameCoordinator = new GameCoordinator();
+    });
 
-    global.document = {
-        getElementById: ()=> {
-            return {
-                appendChild: ()=> { return; }
-            };
-         },
-        createElement: ()=> {
-            return {
-                classList: {
-                    add: ()=> { return; }
-                },
-                appendChild: ()=> { return; },
-                style: {}
-            };
-        }
-    };
-
-    gameCoordinator = new GameCoordinator();
+    describe('drawMaze', ()=> {
+        it('creates the maze and adds entities for a given maze array', ()=> {
+            let entityList = [];
+            gameCoordinator.drawMaze(mazeArray, entityList);
+            assert.strictEqual(entityList.length, 1);
+        });
+    });
 });
-
