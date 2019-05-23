c3cf3981fcf64e72a054ecedbb44d60a0271c42a
Brent <d3wardbrent@gmail.com>
2019-05-23 18:46:40 -0500

    Adding tests

--- app/tests/soundManager.test.js
+++ app/tests/soundManager.test.js
@@ -22,4 +22,41 @@ describe('soundManager', () => {
       assert(spy.calledWith('app/style/audio/some_sound.mp3'));
     });
   });
+
+  describe('setAmbience', () => {
+    const arraySpy = sinon.fake();
+    const connectSpy = sinon.fake();
+    const startSpy = sinon.fake();
+
+    beforeEach(() => {
+      global.fetch = sinon.fake.returns({
+        arrayBuffer: arraySpy,
+      });
+      comp.ambience.decodeAudioData = sinon.fake();
+      comp.ambience.createBufferSource = sinon.fake.returns({
+        connect: connectSpy,
+        start: startSpy,
+      });
+    });
+
+    it('loops an ambient sound', async () => {
+      await comp.setAmbience('some_sound');
+      assert(global.fetch.calledWith('app/style/audio/some_sound.mp3'));
+      assert(arraySpy.called);
+      assert(comp.ambience.decodeAudioData.called);
+      assert(comp.ambience.createBufferSource.called);
+      assert(connectSpy.calledWith(comp.ambience.destination));
+      assert.strictEqual(comp.source.loop, true);
+      assert(startSpy.called);
+    });
+
+    it('stops previously running ambience', () => {
+      comp.source = {
+        stop: sinon.fake(),
+      };
+
+      comp.setAmbience('some_sound');
+      assert(comp.source.stop.called);
+    });
+  });
 });
