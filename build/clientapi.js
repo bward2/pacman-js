496eadb4aca8557186071e1870e7f02fa54615fe
tomat-suppe <slak@itu.dk>
2026-02-09 12:04:50 +0100

    Added support for click events, as well as logging events and their time relate to start, to a list
    Co-authored-by: Ida Amalie Stougaard <iast@itu.dk>
    Co-authored-by: Lukas Hammerhøj <lukas.hammerhoj@gmail.com>

--- build/clientapi.js
+++ build/clientapi.js
@@ -1,3 +1,29 @@
+const EventLog = []; // Array for holding tuples of eventName and eventTime, to send to server
+
+// Listener that triggers on a key-press
 window.addEventListener("keydown", e => {
-   console.log(`Logged ${e.key} action`);
+   const Event = {
+         eventName: e.key,
+         eventX: null, // Client x and y are null. This does not need to be added in JS,
+                       // but has been added for database coherency. Can consider removing.
+         eventY: null,
+         eventTime: e.timeStamp, // MS since browser load
+   };
+
+   EventLog.push(Event); // Adding event name and time to the EventLog list.
+
+   console.log(`Logged ${Event.eventName} action. Logged at ${Event.eventTime}`); // Only for testing
+});
+
+window.addEventListener("click", e => {
+   const Event = {
+      eventName: "click",
+      eventX: e.clientX, // Position of mouse at time of click.
+      eventY: e.clientY,
+      eventTime: e.timeStamp,
+   }
+
+   EventLog.push(Event); // Adding event name and time to the EventLog list.
+   
+   console.log(`Logged ${Event.eventName} action. Logged at ${Event.eventTime}`); // Only for testing
 });
